a2aa2d8b941bc853f24ea72b068d973b20ef78e8
Teriuihi <teriuihi@alttd.com>
2021-07-28 22:50:58 +0200

    Hardcoded 3 character limit for repeated characters (should be configurable)

--- api/src/main/java/com/alttd/chat/managers/RegexManager.java
+++ api/src/main/java/com/alttd/chat/managers/RegexManager.java
@@ -6,10 +6,13 @@ import com.alttd.chat.util.ALogger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegexManager {
 
     private static List<ChatFilter> chatFilters;
+    private static final Pattern pattern = Pattern.compile("(.)\\1{4,}");
 
     public static void initialize() {
         chatFilters = new ArrayList<>();
@@ -35,6 +38,13 @@ public class RegexManager {
                     }
                     break;
             }
+        }
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String group = matcher.group();
+            System.out.println(group);
+            text = text.replace(group, group.substring(0, 3)); //TODO HARD CODED PLS PUT IN CONFIG
+            System.out.println(text);
         }
         return text;
     }
