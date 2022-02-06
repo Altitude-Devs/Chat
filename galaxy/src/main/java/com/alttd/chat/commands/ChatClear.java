08a695f2ac959e00da66bc0cccf75a9f74b62d5a
Teriuihi <teriuihi@alttd.com>
2022-02-06 03:31:07 +0100

    Added chatclear command

---
+++ galaxy/src/main/java/com/alttd/chat/commands/ChatClear.java
@@ -0,0 +1,21 @@
+package com.alttd.chat.commands;
+
+import com.alttd.chat.util.Utility;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ChatClear implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("chat.command.clear-chat")) {
+            sender.sendMessage(Utility.parseMiniMessage("<red>You don't have permission to use this command.</red>"));
+            return true;
+        }
+        Bukkit.getServer().sendMessage(MiniMessage.miniMessage().deserialize("\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n\n \n"));
+        return true;
+    }
+}
