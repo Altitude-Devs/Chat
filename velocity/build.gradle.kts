2ac0cbc7d97385ded92976b1041fcf80045fed47
destro174 <40720638+destro174@users.noreply.github.com>
2022-02-20 19:07:08 +0100

    Remove proxydiscordlink implementation

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -10,13 +10,12 @@ dependencies {
 //    compileOnly("com.velocitypowered:velocity-brigadier:1.0.0-SNAPSHOT")
     implementation("mysql:mysql-connector-java:8.0.27") // mysql
     implementation("org.spongepowered", "configurate-yaml", "4.1.2")
-    implementation("com.alttd.proxydiscordlink:ProxyDiscordLink:1.0.0-BETA-SNAPSHOT")
+    compileOnly("com.alttd.proxydiscordlink:ProxyDiscordLink:1.0.0-BETA-SNAPSHOT")
     implementation("net.kyori", "adventure-text-minimessage", "4.10.0-SNAPSHOT") {
         exclude("net.kyori")
         exclude("net.kyori.examination")
     }
     compileOnly("com.gitlab.ruany:LiteBansAPI:0.3.5")
-    compileOnly("com.alttd.proxydiscordlink:ProxyDiscordLink:1.0.0-BETA-SNAPSHOT")
 }
 
 tasks {
