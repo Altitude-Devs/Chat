4e9228526103be22fd436efa897fb35902a03b2c
Teriuihi <teriuihi@alttd.com>
2024-04-06 15:35:08 +0200

    Add throwable to warn and error methods in ALogger
    This update introduces modifications to the 'ALogger.java' file to handle possible exceptions more efficiently. It extends the warn and error functions to include a Throwable parameter, making error logs more informative for easier debugging.

--- api/src/main/java/com/alttd/chat/util/ALogger.java
+++ api/src/main/java/com/alttd/chat/util/ALogger.java
@@ -16,11 +16,19 @@ public class ALogger {
         logger.warn(message);
     }
 
+    public static void warn(String message, Throwable throwable) {
+        logger.warn(message, throwable);
+    }
+
     public static void info(String message) {
         logger.info(message);
     }
 
     public static void error(String message) {
         logger.error(message);
+    }
+
+    public static void error(String message, Throwable throwable) {
+        logger.error(message, throwable);
     }
 }
